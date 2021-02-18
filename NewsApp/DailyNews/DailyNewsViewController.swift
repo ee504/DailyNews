9a688bcf81b180af5efd1ddf8b48d3cadd5e8878
EE <eispro52@gmail.com>
2021-02-18 17:32:33 +0300

    some

---
+++ NewsApp/DailyNews/DailyNewsViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DailyNewsViewController.swift
+//  NewsApp
+//
+//  Created by EE on 18.02.2021.
+//
+
+import UIKit
+
+class DailyNewsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    //@IBOutlet weak var text: UITextField!
+    
+    //@IBAction func button(_ sender: UIButton) {
+        //text.text = "Test"
+    //}
+    
+}
